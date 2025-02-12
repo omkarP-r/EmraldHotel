2d1cbd3e2eef907cebba90733bbe84a38a8124b2
OMKAR <omkar29pawar@gmail.com>
2025-02-12 19:02:57 +0530

    book repo

--- emrald/src/main/java/com/project/emrald/repository/BookingRepository.java
+++ emrald/src/main/java/com/project/emrald/repository/BookingRepository.java
@@ -3,10 +3,11 @@ package com.project.emrald.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import com.project.emrald.entity.Booking;
 
-public interface BookingRepository extends JpaRepository<Booking, Long> {
+public interface BookingRepository extends CrudRepository<Booking, Long> {
 	
 	Optional<Booking> findByBookingConfirmationCode( String confirmationCode);
 
