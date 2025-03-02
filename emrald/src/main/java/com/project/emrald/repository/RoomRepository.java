b46c9c6074bcb9f80f6b311e9ba733186633166e
omkarP-r <omkar29pawar@gmail.com>
2025-03-02 18:22:35 +0530

    room repo

--- emrald/src/main/java/com/project/emrald/repository/RoomRepository.java
+++ emrald/src/main/java/com/project/emrald/repository/RoomRepository.java
@@ -18,7 +18,7 @@ public interface RoomRepository extends JpaRepository<Room, Long>{
 	@Query("SELECT r FROM Room r WHERE r.id NOT IN (SELECT b.room.id FROM Booking b)")
 	List<Room> getAvailableRooms();
 	
-	@Query("SELECT r FROM Room r WHERE r.roomType LIKE %:roomType% AND r.id NOT IN (SELECT bk.room.id FROM Booking bk WHERE" +
-	"(bk.checkInDate <= :checkOutDate) AND (bk.checkOutDate >= :checkInDate))")
-	List<Room> findAvailableRoomsByDateAndTypes(LocalDate checkInDate, LocalDate checkOutDate, String roomType);
+	 @Query("SELECT r FROM Room r WHERE r.roomType LIKE %:roomType% AND r.id NOT IN (SELECT bk.room.id FROM Booking bk WHERE" +
+	            "(bk.checkInDate <= :checkOutDate) AND (bk.checkOutDate >= :checkInDate))")
+	    List<Room> findAvailableRoomsByDateAndTypes(LocalDate checkInDate, LocalDate checkOutDate, String roomType);
 }
